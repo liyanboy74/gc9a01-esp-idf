c122a9627298c522154f0872df13e56eba071753
Esmaeill <liyanboy74@gmail.com>
2021-03-15 18:54:16 +0330

    update for match in dispcolor

--- include/gc9a01.h
+++ include/gc9a01.h
@@ -13,14 +13,8 @@ void GC9A01_SleepMode(uint8_t Mode);
 void GC9A01_DisplayPower(uint8_t On);
 void GC9A01_DrawPixel(int16_t x, int16_t y, uint16_t color);
 void GC9A01_FillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
-
-#if(CONFIG_GC9A01_BUFFER_MODE)
-void        GC9A01_Update();
-uint16_t    GC9A01_GetPixel(int16_t x, int16_t y);
-#endif
-
-#if(CONFIG_GC9A01_CONTROL_BACK_LIGHT_USED)
+void GC9A01_Update();
 void GC9A01_SetBL(uint8_t Value);
-#endif
+uint16_t GC9A01_GetPixel(int16_t x, int16_t y);
 
 #endif
